5f75a3d5e23fcc85fb0ba84609edcff7920bc909
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-03 17:03:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-   return "FizzBuzz"
-  end
-
-  if int % 5 == 0
-   return "Buzz"
-  end
-
-  if int % 3 == 0
-   return "Fizz"
-  end
+end
